48313c84ccec82a7b08f2d8589df2a4fd6ef255f
Shivambembey98 <shivam.bembey@bellblazetech.com>
2025-07-07 14:21:47 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,16 +2,25 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 100000,         // Number of virtual users per node
-  duration: '5m',     // Duration of the test
+  stages: [
+    { duration: '1m', target: 500 },    // ramp-up to 500 VUs
+    { duration: '3m', target: 500 },    // hold at 500 VUs
+    { duration: '1m', target: 0 },      // ramp-down
+  ],
   thresholds: {
-    http_req_duration: ['p(95)<500'],  // 95% of requests should be < 500ms
-    http_req_failed: ['rate<0.01'],    // Errors should be < 1%
+    http_req_duration: ['p(95)<500'],
+    http_req_failed: ['rate<0.01'],
+  },
+  ext: {
+    influxdb: {
+      pushInterval: '5s', // push every 5 seconds (avoid bulk)
+      tags: { test_env: 'uat' }
+    }
   }
 };
 
 export default function () {
-  const res = http.get('https://test.k6.io');
+  const res = http.get('https://uat.rentte.com/');
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
